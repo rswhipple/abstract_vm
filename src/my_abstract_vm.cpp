cb1dc2e05eab6dad6dfcbc9079f2ab0ca25fccaa
Rebecca W Silverstein <rswhipple@gmail.com>
2024-07-23 07:41:33 -0700

    move template to headers

--- src/my_abstract_vm.cpp
+++ src/my_abstract_vm.cpp
@@ -1,21 +1,5 @@
+
+#include "../inc/main.hpp"
 #include <iostream>
-using namespace std;
 
 
-template <typename T> class IOperand
-{
-public:
-
-  virtual std::string const & toString() const = 0;
-
-  virtual int           getPrecision() const = 0;
-  virtual eOperandType  getType() const = 0;
-
-  virtual IOperand *  operator+(const IOperand &rhs) const = 0;
-  virtual IOperand *  operator-(const IOperand &rhs) const = 0;
-  virtual IOperand *  operator*(const IOperand &rhs) const = 0;
-  virtual IOperand *  operator/(const IOperand &rhs) const = 0;
-  virtual IOperand *  operator%(const IOperand &rhs) const = 0;
-
-  virtual ~IOperand() {}
-};
