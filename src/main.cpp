80307cbc92ad0a2d349e611d177994de4a0d2e99
Rebecca W Silverstein <rswhipple@gmail.com>
2024-07-25 12:24:10 -0700

    remove parse() to simplify

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,14 @@ int main(int argc, char* argv[])
     t_node command = {"temp", 0, 0, NULL};
     const t_node* cmdPtr = &command;
 
+    return EXIT_SUCCESS;
     if (argc > 2) {
         std::cerr << "Usage: " << argv[0] << " [filename]" << std::endl;
         return EXIT_FAILURE;  
     } else if (argc > 1) {
-        parse(argv[1], cmdPtr);
+        readFile(argv[1], cmdPtr);
     } else {
-        parse(NULL, cmdPtr);
+        readFromStdin(cmdPtr);
     }
 
 
--- src/parse.cpp
+++ src/parse.cpp
@@ -12,7 +12,7 @@ const int parse(char *filename, const t_node*& cmdPtr) {
     return EXIT_SUCCESS;
 }
 
-const t_node*& readFile(const std:: string& filename, const t_node*& cmdPtr)
+void readFile(const std:: string& filename, const t_node*& cmdPtr)
 {
     std::ifstream file(filename);
 
@@ -29,7 +29,7 @@ const t_node*& readFile(const std:: string& filename, const t_node*& cmdPtr)
     file.close();
 }
 
-const t_node*& readFromStdin(const t_node*& cmdPtr)
+void readFromStdin(const t_node*& cmdPtr)
 {
     std::string line;
     while (std::getline(std::cin, line)) {
@@ -37,7 +37,7 @@ const t_node*& readFromStdin(const t_node*& cmdPtr)
     }
 }
 
-const t_node*& strToNode(std::string line)
+void strToNode(std::string line)
 {
 
 }
@@ -80,88 +80,8 @@ const t_node*& strToNode(std::string line)
 //             }
 //         }
 //     }
-    
 //     print_nodes(head);  // TESTING
 //     return head;
-// }
-
-// FILE *create_cor_file(char *filename) {
-//     char *name = replace_ext(filename);
-//     const char *write = "wb";
-//     FILE *cor= fopen(name, write);
-//     return cor;
-// }
-
-// char *replace_ext(char *filename) {
-//     int len = my_strlen(filename);
-//     char *new_ext = "cor";
-//     char *new_filename = init_str(len + 2);
-//     my_strncpy(new_filename, filename, len);
-//     my_strcat(new_filename, new_ext);
-
-//     return new_filename;
-// }
-
-// void remove_line_title(char *dest, char *line, int size) {
-//     my_strcpy(dest, line + size); 
-//     int copied_len = my_strlen(dest);
-//     dest[copied_len - 2] = '\0';
-//     dest[copied_len - 1] = '\0';
-// }
-
-// t_node *string_to_node(char *src, t_prog_size *size) {
-//     t_array *tokens = tokenizer(src, SEPARATOR_SET);
-
-//     t_node *args = init_node(4);
-//     int i = 0;
-    
-//     int tok_len = my_strlen(tokens->array[0]);
-//     if (tokens->array[0][tok_len - 1] == LABEL_CHAR) {
-//         args->label = tokens->array[0];
-//         args->label[tok_len - 1] = '\0';
-//         i++;
-//     }
-
-//     tok_len = my_strlen(tokens->array[i]);
-//     args->command = init_str(tok_len + 1);
-//     my_strcpy(args->command, tokens->array[i]);
-//     i++;
-
-//     if (is_special_command(args->command)) args->num_bytes -= 1;
-
-//     while (tokens->array[i]) {   
-//         if (tokens->array[i][0] == COMMENT_CHAR) continue;
-//         else if (tokens->array[i][0] == 'r') {
-//             ttoa_remove_char(&args, tokens->array[i], 1);
-//             args->num_bytes++;
-//             args->param_count++;
-//         }
-//         else if (tokens->array[i][0] == DIRECT_CHAR) {
-//             ttoa_remove_char(&args, tokens->array[i], 2);
-//             args->num_bytes += 4;
-//             args->param_count++;
-//         }
-//         else {
-//             token_to_arg(&args, tokens->array[i], 3);
-//             args->num_bytes += 2;
-//             args->param_count++;
-//         }
-//         i++;
-//     }
-    
-//     size->total_bytes += (args->num_bytes);
-//     free_t_array(tokens);
-
-//     return args;
-// }
-
-// void token_to_arg(t_node **args, char *tok, int type) 
-// {
-//     (*args)->array[(*args)->param_count] = init_arg();
-//     t_arg *tmp = (*args)->array[(*args)->param_count];
-//     tmp->arg = init_str(my_strlen(tok) + 1);
-//     my_strcpy(tmp->arg, tok);
-//     tmp->type = type;
 // }
 
 
