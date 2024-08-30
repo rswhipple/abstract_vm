9147f549daf4a8e534ec79680cc23dc74fe387de
Rebecca W Silverstein <rswhipple@gmail.com>
2024-08-30 13:45:08 -0700

    remove empty files, simplify main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,14 @@
 
 int main(int argc, char* argv[])
 {
-    t_node command = {"temp", 0, 0, NULL};
-    const t_node* cmdPtr = &command;
-
-    return EXIT_SUCCESS;
     if (argc > 2) {
         std::cerr << "Usage: " << argv[0] << " [filename]" << std::endl;
         return EXIT_FAILURE;  
     } else if (argc > 1) {
-        if (readFile(argv[1], cmdPtr) != 0) return EXIT_FAILURE;
+        if (readFile(argv[1]) != 0) return EXIT_FAILURE;
     } else {
-        if (readFromStdin(cmdPtr) != 0) return EXIT_FAILURE;
-    }
+        if (readFromStdin() != 0) return EXIT_FAILURE;
+    } 
 
     return EXIT_SUCCESS;
 }
