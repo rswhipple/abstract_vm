41c38d4a6450a83dcc1c92bb709b31488308c342
Rebecca W Silverstein <rswhipple@gmail.com>
2024-09-07 08:30:46 -0700

    ADD OperandFactory class

---
+++ inc/factory.hpp
@@ -0,0 +1,48 @@
+#ifndef FACTORY_HPP
+#define FACTORY_HPP
+
+#include "ioperand.hpp"
+#include <array>
+
+
+// Factory pattern to manage creation of IOperand instances
+class OperandFactory {
+public:
+    using CreateOperandFunc = IOperand* (*)(const std::string&);
+
+private:
+    static IOperand* _createInt8(const std::string& value) {
+        int8_t val = static_cast<int8_t>(std::stoi(value));
+        return new Operand<int8_t, eOperandType::Int8>(val);
+    }
+
+    static IOperand* _createInt16(const std::string& value) {
+        int16_t val = static_cast<int16_t>(std::stoi(value));
+        return new Operand<int16_t, eOperandType::Int16>(val);
+    }
+
+    static IOperand* _createInt32(const std::string& value) {
+        int32_t val = static_cast<int32_t>(std::stoi(value));
+        return new Operand<int32_t, eOperandType::Int32>(val);
+    }
+
+    static IOperand* _createFloat(const std::string& value) {
+        float val = std::stof(value);
+        return new Operand<float, eOperandType::Float>(val);
+    }
+
+    static IOperand* _createDouble(const std::string& value) {
+        double val = std::stod(value);
+        return new Operand<double, eOperandType::Double>(val);
+    }
+
+public:
+    static const std::array<CreateOperandFunc, 5> constructors;
+    
+    IOperand* createOperand(eOperandType type, const std::string& value) const {
+        return constructors[static_cast<size_t>(type)](value);
+    }
+};
+
+
+#endif
--- src/execute.cpp
+++ src/execute.cpp
@@ -94,19 +94,10 @@ void executeDump(std::list<IOperand*>& stk)
 int executePush(std::list<IOperand*>& stk, std::string typ, std::string val)
 {
     eOperandType type = stringToType(typ);
-    IOperand* operand = nullptr;
+    OperandFactory factory;
 
-    if (type == eOperandType::Int8)
-        operand = new Operand<int8_t, eOperandType::Int8>(static_cast<int8_t>(std::stoi(val)));
-    else if (type == eOperandType::Int16)
-        operand = new Operand<int16_t, eOperandType::Int16>(static_cast<int16_t>(std::stoi(val)));
-    else if (type == eOperandType::Int32)
-        operand = new Operand<int32_t, eOperandType::Int32>(static_cast<int32_t>(std::stoi(val)));
-    else if (type == eOperandType::Float)
-        operand = new Operand<float, eOperandType::Float>(std::stof(val));
-    else if (type == eOperandType::Double)
-        operand = new Operand<double, eOperandType::Double>(std::stod(val));
-    else errorHandler(errorType::invalidArg);
+    // Create Operand using factory pattern
+    IOperand* operand = factory.createOperand(type, val);
     
     stk.push_front(operand);
 
@@ -124,25 +115,33 @@ int executeArithmetic(std::list<IOperand*>& stk, Cmd op)
 {
     if (static_cast<int>(stk.size()) < 2) return 6;   // errorType::stackUnderflow
 
+    OperandFactory factory;
     IOperand* operand = nullptr;
-    IOperand* lhs = stk.front();
-    stk.pop_front();
     IOperand* rhs = stk.front();
     stk.pop_front();
+    IOperand* lhs = stk.front();
+    stk.pop_front();
+    
 
     // Check for precision
     // Turn the operand that is a lower precision into the higher one
+    if (lhs->getType() != rhs->getType()) {
+        if (!lhsIsMostPrecise(lhs, rhs)) {
+            eOperandType e = rhs->getType();
+            lhs = factory.createOperand(e, lhs->toString());
+        } else {
+            eOperandType e = lhs->getType();
+            rhs = factory.createOperand(e, rhs->toString());
+        }
+    }
+    
     // Perform arithmetic
 
-    /* 
-       For non commutative operations, you must consider for the following stack: 
-       v1 on v2, the calculation in infix notation: v2 op v1.
-    */ 
-    if (op == Cmd::Add) operand = *rhs + *lhs;
-    else if (op == Cmd::Sub) operand = *rhs - *lhs;
-    else if (op == Cmd::Mul) operand = *rhs * *lhs;
-    else if (op == Cmd::Div) operand = *rhs / *lhs;
-    else if (op == Cmd::Mod) operand = *rhs % *lhs;
+    if (op == Cmd::Add) operand = *lhs + *rhs;
+    else if (op == Cmd::Sub) operand = *lhs - *rhs;
+    else if (op == Cmd::Mul) operand = *lhs * *rhs;
+    else if (op == Cmd::Div) operand = *lhs / *rhs;
+    else if (op == Cmd::Mod) operand = *lhs % *rhs;
 
     // TODO add an error check here, compare regular math to operand math
     stk.push_front(operand);
